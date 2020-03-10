2e245acedf482e4670a17f5b88ca0be1e86bc0e9
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2020-03-10 13:33:37 +0100

    Simple Movement and Collisions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 1.0f;
+    private Rigidbody playerRigidbody;
+    public ForceMode forceMode = ForceMode.Acceleration;
+    private Vector3 playerInput;
+
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        playerRigidbody.velocity = playerInput * movementSpeed;
+    }
+}
