0c8e831c9cdc7220d0dfbda5874187b664b70825
Piotr <gortyn98@gmail.com>
2020-03-10 18:41:01 +0100

    Enemy Alpha

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float maxHealth;
+    [SerializeField]
+    private float damage;
+    [SerializeField]
+    private float health;
+    public float MaxHealth { get => maxHealth; }
+    public float Damage { get => damage; }
+    public float Health { get => health; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : Character
+{
+    private Rigidbody rigidbody;
+    [SerializeField]
+    private float speed = 1.0f;
+    [SerializeField]
+    private float velocityX;
+    [SerializeField]
+    private float velocityY;
+    [SerializeField]
+    private float fieldOfView;
+    [SerializeField]
+    private float neighbourDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //rigidbody.velocity = speed * 
+    }
+}
