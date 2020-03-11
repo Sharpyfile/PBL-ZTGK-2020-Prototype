d5f6c795a6004cbe601ccb441e63bdba83342668
Sharpyfile <44209792+Sharpyfile@users.noreply.github.com>
2020-03-11 22:38:12 +0100

    Basic pills affecting speed with set duration time

---
+++ Assets/Scripts/PickupStatistics.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupStatistics : MonoBehaviour
+{
+    public float durationTime = 5.0f;
+    public float speedModifier = 0.5f;
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float movementSpeed = 1.0f;
+    public float basicMovementSpeed;
+    public float finalMovementSpeed;
     private Rigidbody playerRigidbody;
     public ForceMode forceMode = ForceMode.Acceleration;
     private Vector3 playerInput;
+    public GameObject pickup;
+    private float timeStamp;
 
     void Start()
     {
@@ -18,6 +21,46 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        playerRigidbody.velocity = playerInput * movementSpeed;
+        playerRigidbody.velocity = playerInput * finalMovementSpeed;
+
+        if (pickup != null)
+        {
+            HasPickup(); //Contains all functions that are done when player has pickup
+        }        
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Pickup") && pickup == null)
+        {
+            Debug.Log("Pickup detected");
+            pickup = other.gameObject;
+            timeStamp = Time.time + pickup.GetComponent<PickupStatistics>().durationTime;
+            Debug.Log(timeStamp);
+        }            
+    }
+    
+    private void StatisticsChangeByPickup() //Changes statistics of the player
+    {
+        if (timeStamp > Time.time)
+        {
+            finalMovementSpeed = basicMovementSpeed * pickup.GetComponent<PickupStatistics>().speedModifier;
+            Debug.Log("Pickup affecting speed" + (timeStamp - Time.time).ToString());
+        }          
+        
+        if (timeStamp < Time.time)
+        {
+            finalMovementSpeed = basicMovementSpeed;
+            Destroy(pickup.gameObject);
+            pickup = null;
+        }         
+    }
+
+    private void HasPickup() //Contains all
+    {
+        pickup.transform.position = playerRigidbody.transform.position;
+        pickup.transform.position = pickup.transform.position + new Vector3(0, 1, 0);
+        StatisticsChangeByPickup();
+    }
+
 }
