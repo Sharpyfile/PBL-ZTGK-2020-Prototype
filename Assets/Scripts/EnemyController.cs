b50ff0f52798201f367e7bf59b4065b5fb2f5baf
Piotr <gortyn98@gmail.com>
2020-03-24 19:21:32 +0100

    EnemyAI

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,16 +6,25 @@ public class EnemyController : Character
 {
     private Rigidbody rigidbody;
     [SerializeField]
-    private float speed = 1.0f;
+    private float speed = 10.0f;
     [SerializeField]
-    private float velocityX;
+    private float fieldOfView = 90.0f;
     [SerializeField]
-    private float velocityY;
-    [SerializeField]
-    private float fieldOfView;
-    [SerializeField]
-    private float neighbourDistance;
+    private float neighbourDistance = 10.0f;
     private GameObject[] otherEnemies;
+    [SerializeField]
+    private float wage = 0.01f;
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private float rotationSpeed = 3f;
+    [SerializeField]
+    private float MinimalDistance = 8.0f;
+
+    private float sum = 0;
+    private float avDist;
+    private List<GameObject> neighbours;
+    private bool leader;
     // Start is called before the first frame update
 
     private MeshRenderer ren;
@@ -29,13 +38,39 @@ public class EnemyController : Character
         rigidbody = GetComponent<Rigidbody>();
         ren = GetComponentInChildren<MeshRenderer>();
         anim = GetComponent<Animator>();
+        neighbours = new List<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        sum = 0;
+        neighbours.Clear();
         otherEnemies = GameObject.FindGameObjectsWithTag("Enemy");
-        //rigidbody.velocity = speed * 
+        foreach (var e in otherEnemies)
+        {
+            if (IsInNeighbourhood(e) && e != this.gameObject)
+            {
+                neighbours.Add(e);
+                sum += Distance(e);
+            }
+        }
+        avDist = sum / neighbours.Count;
+
+        Debug.Log(neighbours.Count);
+
+        if(!leader)
+        {
+            Debug.Log(name + " I'm not a leader");
+            BeInTheMiddle();
+            PersonalSpace();
+        }
+        else
+        {
+            Debug.Log(name + "I'M A LEADER!!!");
+        }
+
+        Move();
     }
 
     void OnTriggerEnter(Collider other)
@@ -78,5 +113,101 @@ public class EnemyController : Character
     public void TurnEnemyWhite()
     {
         ren.material.color = Color.green;
+    }
+
+    bool IsInNeighbourhood(GameObject enemy)
+    {
+        float k1, k2;
+        if (Distance(enemy) < neighbourDistance)
+        {
+            if (rigidbody.velocity.x != 0)
+            {
+                k1 = Mathf.Atan(rigidbody.velocity.z / rigidbody.velocity.x);
+            }
+            else
+            {
+                k1 = Mathf.Atan(rigidbody.velocity.z);
+            }
+            if (enemy.GetComponent<Rigidbody>().velocity.x - rigidbody.velocity.x != 0)
+            {
+                k2 = Mathf.Atan((enemy.GetComponent<Rigidbody>().velocity.z - rigidbody.velocity.z) /
+                                      (enemy.GetComponent<Rigidbody>().velocity.x - rigidbody.velocity.x));
+            }
+            else if (Vector3.Dot(enemy.transform.position - transform.position, enemy.transform.forward) == -1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            if (Mathf.Abs(k1 - k2) < fieldOfView)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private float Distance(GameObject enemy)
+    {
+        return Mathf.Sqrt(((this.transform.position.x - enemy.transform.position.x) * (this.transform.position.x - enemy.transform.position.x)) +
+            ((this.transform.position.z - enemy.transform.position.z) * (this.transform.position.z - enemy.transform.position.z)));
+    }
+
+    private void Move()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+                                               Quaternion.LookRotation(player.position - transform.position),
+                                               rotationSpeed * Time.deltaTime);
+        rigidbody.velocity += transform.forward * speed * Time.deltaTime;
+
+    }
+
+    private void BeInTheMiddle()
+    {
+        float dist, newVelocityX, newVelocityZ;
+        foreach (var n in neighbours)
+        {
+            dist = Distance(n);
+            newVelocityX = rigidbody.velocity.x + wage * (((n.transform.position.x - transform.position.x) * (dist - avDist)) / dist);
+            newVelocityZ = rigidbody.velocity.z + wage * (((n.transform.position.z - transform.position.z) * (dist - avDist)) / dist);
+            Vector3 newVelocity = new Vector3(newVelocityX, 0, newVelocityZ);
+            rigidbody.velocity = newVelocity;
+        }
+    }
+
+    private void PersonalSpace()
+    {
+        float dist, newVelocityX, newVelocityZ;
+        foreach (var n in neighbours)
+        {
+            dist = Distance(n);
+            newVelocityX = rigidbody.velocity.x - wage * ((((n.transform.position.x - transform.position.x) * MinimalDistance) / dist) - (n.transform.position.x - transform.position.x));
+            newVelocityZ = rigidbody.velocity.z - wage * ((((n.transform.position.z - transform.position.z) * MinimalDistance) / dist) - (n.transform.position.z - transform.position.z));
+            Vector3 newVelocity = new Vector3(newVelocityX, 0, newVelocityZ);
+            rigidbody.velocity = newVelocity;
+        }
+    }
+
+    public void SetLeader()
+    {
+        foreach(var e in neighbours)
+        {
+            e.GetComponent<EnemyController>().UnSetLeader();
+        }
+        leader = true;
+    }
+
+    public void UnSetLeader()
+    {
+        leader = false;
     }
 }
---
+++ Assets/Scripts/LeaderChooser.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderChooser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject[] enemies;
+    private bool skip;
+    private int value;
+    private List<float> distances;
+    [SerializeField] private GameObject player;
+    void Start()
+    {
+        distances = new List<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        skip = false;
+        distances.Clear();
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var e in enemies)
+        {
+            distances.Add(Distance(e, player));
+        }
+        enemies[ShortestDistance()].GetComponent<EnemyController>().SetLeader();
+    }
+
+    private float Distance(GameObject enemy, GameObject player)
+    {
+        return Mathf.Sqrt(((player.transform.position.x - enemy.transform.position.x) * (player.transform.position.x - enemy.transform.position.x)) +
+            ((player.transform.position.z - enemy.transform.position.z) * (player.transform.position.z - enemy.transform.position.z)));
+    }
+
+    private int ShortestDistance()
+    {
+        float min = distances[0];
+        int index = 0;
+        for (int i = 1; i < distances.Count; i++)
+        {
+            if (distances[i] < min)
+            {
+                min = distances[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
